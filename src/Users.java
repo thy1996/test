b45ff657cacc081abd7ddf2d1479ef1e23c59ce8
LifeStone <31197244+LifeStone@users.noreply.github.com>
2017-08-23 15:23:24 +0800

    class

--- src/Users.java
+++ src/Users.java
@@ -1,5 +1,4 @@
 public class Users {
-     class users {
         String Account;
         String password;
         String Name;
@@ -10,46 +9,4 @@ public class Users {
         void login(){}
         void cancellation(){}
     }
-    class writer extends users {
-        private String Date;
-        private String Adress;
-        private  String Referees;
-        private  String Num;
-        private  String Title;
-        private  String Position;
-        private  String Email;
-        private  String Wechat;
 
-
-
-        String getDate(){ return Date;}
-        String getAdress(){return Adress;}
-        String getReferees(){return Referees;}
-        String getNum(){return Num;}
-        String getTitle(){return Title;}
-        String getPosition(){return Position;}
-        String getEmail(){return Email;}
-        String getWechat(){return Wechat;}
-        void Billquery(){}//提案查询
-        void Proposalpreparation(){}//提案编制
-        void Standardquery(){}
-        void Proposalquery(){}
-        void Informationmaintenance(){}
-
-
-
-
-
-
-
-
-    }
-
-
-
-
-
-
-
-
-}
---
+++ src/Writer.java
@@ -0,0 +1,33 @@
+class Writer extends Users {
+    private String Date;
+    private String Adress;
+    private  String Referees;
+    private  String Num;
+    private  String Title;
+    private  String Position;
+    private  String Email;
+    private  String Wechat;
+
+    String getDate(){ return Date;}
+    String getAdress(){return Adress;}
+    String getReferees(){return Referees;}
+    String getNum(){return Num;}
+    String getTitle(){return Title;}
+    String getPosition(){return Position;}
+    String getEmail(){return Email;}
+    String getWechat(){return Wechat;}
+    void setDate(String rep){Date=rep;}
+    void setAdress(String rep){Adress=rep;}
+    void setReferees(String rep){ Referees=rep;}
+    void setNum(String rep){ Num=rep;}
+    void setTitle(String rep){ Title=rep;}
+    void setPosition(String rep){ Position=rep;}
+    void setEmail(String rep){ Email=rep;}
+    void setWechat(String rep){ Wechat=rep;}
+    void Billquery(){}//提案查询
+    void Proposalpreparation(){}//提案编制
+    void Standardquery(){}
+    void Proposalquery(){}
+    void Informationmaintenance(){}
+
+}
